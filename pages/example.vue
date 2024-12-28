6b4050d390fbe0edb89664db59fdac2afd31ae7f
Noe0131 <168168800+Noe0131@users.noreply.github.com>
2024-12-28 22:48:04 +0900

    真ファイルアップ

---
+++ pages/example.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style>
+.greeting {
+  color: royalblue;
+  font-weight: bold;
+  font-size: calc(16px + 2vw);
+}
+</style>
