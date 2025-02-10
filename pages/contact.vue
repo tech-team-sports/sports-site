c911cb6ab013e3f7b39cff125386b652beb00216
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2025-02-10 12:08:47 +0900

    お問い合わせページ作成

---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="contact-container">
+    <h1 class="contact-title">お問い合わせ</h1>
+    <ContactForm />
+  </div>
+</template>
+
+<script setup>
+import ContactForm from '../components/ContactForm/ContactForm.vue'
+</script>
+
+<style scoped>
+.contact-container {
+  padding: 1.5rem;
+}
+
+.contact-title {
+  font-size: 3rem; /* フォントサイズを大きく */
+  font-weight: bold;
+  text-align: center; /* 中央揃え */
+  margin-bottom: 1rem; /* 下に少しスペース */
+}
+</style>
