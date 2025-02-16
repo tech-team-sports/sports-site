027f5a4d11742674a4fc2faf9d975c0cca438efd
Noe0131 <168168800+Noe0131@users.noreply.github.com>
2025-02-16 22:03:41 +0900

    Deploy Nuxt site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,11 @@ export default defineNuxtConfig({
     preset: "static", 
   },
   compatibilityDate: "2025-02-15",
+
+  // Add baseURL for GitHub Pages
+  app: {
+    baseURL: "/sports-site/", // Change this to match your GitHub repository name
+    cdnURL: "https://tech-team-sports.github.io/sports-site/" // Force absolute paths
+  },
 })
+
