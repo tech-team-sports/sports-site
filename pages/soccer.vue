e49701ac20c8497a1fc23321191e42f5c4dde94b
Noe0131 <168168800+Noe0131@users.noreply.github.com>
2025-02-25 21:46:59 +0900

    d

--- pages/soccer.vue
+++ pages/soccer.vue
@@ -1,18 +1,18 @@
 <template>
+  <div>
     <soccerSlider />
     <soccerLinks />
     <rulu />
+  </div>
 </template>
   
 <script setup>
   import soccerSlider from "~/components/soccer/Slider/soccerSlider.vue";
   import soccerLinks from "~/components/soccer/links/soccer-links.vue";
   import rulu from "~/components/soccer/rule/rulu.vue";
-  
-
 </script>
 
 <style scoped>
-
 </style>
+
   
